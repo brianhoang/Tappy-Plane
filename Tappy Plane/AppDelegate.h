aa6f3b9ae24d3e86821f10297d4ac7022071ab34
Brian Hoang <bbhoang@ucdavis.edu>
2014-12-26 20:18:48 -0800

    Initial Commit

---
+++ Tappy Plane/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tappy Plane
+//
+//  Created by Brian Hoang on 12/26/14.
+//  Copyright (c) 2014 Brian Hoang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tappy Plane/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Tappy Plane
+//
+
+//  Copyright (c) 2014 Brian Hoang. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
